ad6564d764f69650cfde3130c59a65f3aaeac696
Swaroop Sridhar <Swaroop.Sridhar@microsoft.com>
2019-02-06 17:15:04 -0800

    Setup build Framework for bundler implementation

---
+++ src/managed/tools/bundle/bundle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace bundle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
