9aa2b773dd5b39de8c7bae8fd39da2b4b18c174f
Swaroop Sridhar <swaroop.sridhar@microsoft.com>
2019-07-03 00:49:31 -0700

    Mac editor

---
+++ src/managed/Microsoft.NET.HostModel/MachO/Header.cs
@@ -0,0 +1,34 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System;
+using System.IO;
+
+namespace Microsoft.NET.HostModel.MachO
+{
+    public struct MachHeader
+    {
+        uint magic;
+        int cputype;
+        int cpusubtype;
+        uint filetype;
+        uint ncmds;
+        uint sizeofcmds;
+        uint flags;
+    };
+
+    public struct mach_header_64
+    {
+        uint magic;
+        int cputype;
+        int cpusubtype;
+        uint filetype;
+        uint ncmds;
+        uint sizeofcmds;
+        uint flags;
+        uint reserved;
+    };
+
+}
+
